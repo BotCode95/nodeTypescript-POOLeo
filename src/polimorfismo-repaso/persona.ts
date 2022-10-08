9c7bcfdbc1ee3df8e616e85620b3b5c799fa5acb
pbottino <pbottino95@gmail.com>
2022-10-08 16:16:30 -0300

    ;

--- src/polimorfismo-repaso/persona.ts
+++ src/polimorfismo-repaso/persona.ts
@@ -9,16 +9,16 @@ class Persona {
         this.nombre = nombre;
         this.apellido = apellido;
         this.edad= edad;
-        this.alturaCM= 0
-        this.pesoKG=0
+        this.alturaCM= 0;
+        this.pesoKG=0;
     }
 
     pesoPromedio():void{
-        this.alturaCM= 50
+        this.alturaCM= 50;
     }
 
     alturaPromedio():void {
-        this.pesoKG = 3
+        this.pesoKG = 3;
     }
 
 }
@@ -26,30 +26,30 @@ class Persona {
 
 class Niño extends Persona {
     constructor(nombre:string, apellido: string, edad: number){
-        super(nombre,apellido,edad)
+        super(nombre,apellido,edad);
     }
 
     pesoPromedio():void{
-        this.alturaCM= 118
+        this.alturaCM= 118;
     }
 
     alturaPromedio() {
-        this.pesoKG = 28
+        this.pesoKG = 28;
     }
 
 }
 
 class Adolescente extends Persona {
     constructor(nombre:string, apellido: string, edad: number){
-        super(nombre,apellido,edad)
+        super(nombre,apellido,edad);
     }
 
     pesoPromedio(){
-        this.alturaCM= 155
+        this.alturaCM= 155;
     }
 
     alturaPromedio():void {
-        this.pesoKG = 50
+        this.pesoKG = 50;
     }
 
 }
@@ -57,33 +57,33 @@ class Adolescente extends Persona {
 
 class Adulto extends Persona {
     constructor(nombre:string, apellido: string, edad: number){
-        super(nombre,apellido,edad)
+        super(nombre,apellido,edad);
     }
 
     pesoPromedio():void{
-        this.alturaCM= 180
+        this.alturaCM= 180;
     }
 
     alturaPromedio():void {
-        this.pesoKG = 78
+        this.pesoKG = 78;
     }
 
 }
 
 
-const niño=new Niño('Nahuel', 'Rosales', 8)
-const adolescente=new Adolescente('Joaquín', 'Madiel',14)
-const adulto=new Adulto('Jorge', 'Alzagas',29)
+const niño=new Niño('Nahuel', 'Rosales', 8);
+const adolescente=new Adolescente('Joaquín', 'Madiel',14);
+const adulto=new Adulto('Jorge', 'Alzagas',29);
 
-niño.alturaPromedio() 
-niño.pesoPromedio()
-adolescente.alturaPromedio() 
-adolescente.pesoPromedio()
-adulto.alturaPromedio() 
-adulto.pesoPromedio()
+niño.alturaPromedio();
+niño.pesoPromedio();
+adolescente.alturaPromedio();
+adolescente.pesoPromedio();
+adulto.alturaPromedio();
+adulto.pesoPromedio();
 
-console.log(niño)
-console.log(adolescente)
-console.log(adulto)
+console.log(niño);
+console.log(adolescente);
+console.log(adulto);
 
 
--- src/polimorfismo-repaso/personaEmpresa.ts
+++ src/polimorfismo-repaso/personaEmpresa.ts
@@ -5,34 +5,34 @@ class PersonaEmp {
  private edad : number;
 
  constructor(id: number, nombre: string, apellido: string, edad: number){
-    this.id = id,
-    this.nombre =nombre,
-    this.apellido = apellido,
-    this.edad =edad
+    this.id = id;
+    this.nombre =nombre;
+    this.apellido = apellido;
+    this.edad =edad;
  }
  private edadPersona():string{ 
-    return `La edad de la persona es ${this.edad}`
+    return `La edad de la persona es ${this.edad}`;
  }
  public mostrarDatos():void {
     const edadPersona = this.edadPersona()
-    console.log(`La persona es ${this.nombre} ${this.apellido} y ${edadPersona} `)
+    console.log(`La persona es ${this.nombre} ${this.apellido} y ${edadPersona} `);
  }
 
 
 }
               //extends = Heredar
 class Empleado extends PersonaEmp {
-    private sueldo: number
-    private experienciaLaboral : number
+    private sueldo: number;
+    private experienciaLaboral : number;
     constructor(id:number, nombre: string, apellido: string, edad: number, sueldo: number, experienciaLaboral: number){
         //Super contructor de la clase Padre
-        super(id,nombre,apellido,edad)
-        this.sueldo = sueldo
-        this.experienciaLaboral=experienciaLaboral
+        super(id,nombre,apellido,edad);
+        this.sueldo = sueldo;
+        this.experienciaLaboral=experienciaLaboral;
     }
 
     mostrarSueldo() {
-        console.log(`El sueldo de la persona es de ${this.sueldo}`)
+        console.log(`El sueldo de la persona es de ${this.sueldo}`);
     }
 
 } 
@@ -41,63 +41,63 @@ class Cliente extends PersonaEmp{
     private fidelidad: string;
     constructor(id:number, nombre: string, apellido: string, edad: number,fidelidad: string){
         //Super contructor de la clase Padre
-        super(id,nombre,apellido,edad)
-        this.fidelidad = fidelidad
+        super(id,nombre,apellido,edad);
+        this.fidelidad = fidelidad;
     }
 
   
 }
 
 
-const personaEmp = new PersonaEmp(3, 'Matias', 'Jael', 25)
-const empleado11 = new Empleado(1, 'Juan', 'Bottino', 27, 200, 2)
-const empleado12 = new Empleado(4, 'Juan', 'Saavedra', 27, 200, 2)
-const cliente11 = new Cliente(2, 'Pablo', 'Guard', 22, 'Cliente de Vea')
-empleado11.mostrarDatos()
-empleado11.mostrarSueldo()
-cliente11.mostrarDatos()
+const personaEmp = new PersonaEmp(3, 'Matias', 'Jael', 25);
+const empleado11 = new Empleado(1, 'Juan', 'Bottino', 27, 200, 2);
+const empleado12 = new Empleado(4, 'Juan', 'Saavedra', 27, 200, 2);
+const cliente11 = new Cliente(2, 'Pablo', 'Guard', 22, 'Cliente de Vea');
+empleado11.mostrarDatos();
+empleado11.mostrarSueldo();
+cliente11.mostrarDatos();
 
 
 class Empresa{
-    private razonSocial:string
-    private fechaCreacion: string
-    public listadoClientes: Cliente[]
-    public listadoEmpleados: Empleado[]
+    private razonSocial:string;
+    private fechaCreacion: string;
+    public listadoClientes: Cliente[];
+    public listadoEmpleados: Empleado[];
 
     constructor(razonSocial: string, fechaCreacion:string){
-        this.razonSocial= razonSocial
-        this.fechaCreacion = fechaCreacion,
-        this.listadoClientes = []
-        this.listadoEmpleados= []
+        this.razonSocial= razonSocial;
+        this.fechaCreacion = fechaCreacion;
+        this.listadoClientes = [];
+        this.listadoEmpleados= [];
     }
 
     agregarCliente(nuevoCliente: Cliente) {
-        this.listadoClientes.push(nuevoCliente)
+        this.listadoClientes.push(nuevoCliente);
     }
 
     contratarEmpleado(nuevoEmpleado: Empleado) {
-        this.listadoEmpleados.push(nuevoEmpleado)
+        this.listadoEmpleados.push(nuevoEmpleado);
     }
                  
     eliminarCliente(idCliente: number){      
-        return this.listadoClientes.filter(cliente => (cliente.id !== idCliente))
+        return this.listadoClientes.filter(cliente => (cliente.id !== idCliente));
     }
 
     despedirEmpleado(idEmpleado: number){
-        return this.listadoEmpleados.filter(empleado => (empleado.id !== idEmpleado))
+        return this.listadoEmpleados.filter(empleado => (empleado.id !== idEmpleado));
     }
 }
 
 
-const empresa1 = new Empresa('Empresa S.A', '20/10/2000')
+const empresa1 = new Empresa('Empresa S.A', '20/10/2000');
 
 
-empresa1.contratarEmpleado(empleado11)
-empresa1.contratarEmpleado(empleado12)
+empresa1.contratarEmpleado(empleado11);
+empresa1.contratarEmpleado(empleado12);
 
-empresa1.agregarCliente(cliente11)
+empresa1.agregarCliente(cliente11);
 
 // empresa1.listadoEmpleados
 
-console.log(empresa1.listadoEmpleados)
-console.log(empresa1.listadoClientes)
+console.log(empresa1.listadoEmpleados);
+console.log(empresa1.listadoClientes);
